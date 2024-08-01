cf9870c5db6d6f87fefafc028922a1d86ddfbf39
unknown <Abstract Programmer>
2024-08-01 11:55:29 -1000

    first commit

---
+++ SkyLine/Models/Employees.cs
@@ -0,0 +1,11 @@
+﻿namespace SkyLine.Models
+{
+	public class Employees
+	{
+		public string FullName { get; set; }
+		public int Id { get; set; }
+		public string NationalId { get; set; }
+		public string Position { get; set; }
+		public decimal Salary { get; set; }
+	}
+}
---
+++ SkyLine/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SkyLine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
